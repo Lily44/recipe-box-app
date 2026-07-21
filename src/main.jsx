4d01dff019cf5833a12f9618371b90241b0032fd
Lori Olson <lsolson@mac.com>
2026-07-21 16:23:21 -0500

    adding front-end files

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RecipeBox from "./RecipeBox.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RecipeBox />
+  </React.StrictMode>
+);
